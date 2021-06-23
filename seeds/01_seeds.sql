f3928d33511e6f7bb7bf13f0228ceef4794f7158
Sayantan Basu <sayantanbasu@live.in>
2021-06-23 14:17:43 -0400

    fake data

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -3,3 +3,14 @@ VALUES
 (1, 'Sayantan Basu', 'sayantanbasu@live.in', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
 (2, 'Aaron Khorsh', 'aaronk@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
 (3, 'Frito Lays', 'Fritol@yahoo.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+
+INSERT INTO properties (id, description, thumbnail_photo_url, cover_photo_url, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code) VALUES
+(1, 'big house', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 2, 5, 'Canada', 'street', 'province', 'N4F G09'),
+(2, 'small house', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 2, 5, 'Canada', 'street', 'province', 'N4F G09'),
+(3, 'medium house', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 2, 5, 'Canada', 'street', 'province', 'N4F G09') ;
+
+INSERT INTO reservations (guest_id, property_id, start_date, end_date)
+VALUES (1, 1, '2018-09-11', '2018-09-26'),
+(2, 2, '2019-01-04', '2019-02-01'),
+(3, 3, '2021-10-01', '2021-10-14');
