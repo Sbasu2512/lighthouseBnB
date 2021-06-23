1654078263a18b0a957642110130b461462bd3ab
Sayantan Basu <sayantanbasu@live.in>
2021-06-23 14:10:48 -0400

    added data to users

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (id, name, email, password) 
+VALUES 
+(1, 'Sayantan Basu', 'sayantanbasu@live.in', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+(2, 'Aaron Khorsh', 'aaronk@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+(3, 'Frito Lays', 'Fritol@yahoo.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
