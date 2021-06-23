3425ee58e36dea646bf583d6a312161abd554ec9
Sayantan Basu <sayantanbasu@live.in>
2021-06-23 13:49:54 -0400

    created properties table

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -1,4 +1,4 @@
-DROP TABLE users ;
+-- DROP TABLE users IF EXISTS;
 
 CREATE TABLE users (
   id INTEGER PRIMARY KEY NOT NULL,
@@ -7,9 +7,29 @@ CREATE TABLE users (
   password VARCHAR(255) NOT NULL 
 );
 
+-- DROP TABLE properties IF EXISTS; 
+
+CREATE properties(
+id INTEGER PRIMARY KEY NOT NULL,
+owner_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+title VARCHAR(255),
+description TEXT,
+thumbnail_photo_url VARCHAR(255),
+cover_photo_url VARCHAR(255),
+cost_per_night INTEGER,
+parking_spaces INTEGER,
+number_of_bathrooms INTEGER,
+number_of_bedrooms INTEGER,
+country VARCHAR(255),
+street VARCHAR(255),
+city VARCHAR(255),
+province VARCHAR(255),
+post_code VARCHAR(255),
+active BOOLEAN
+);
 
 
-DROP TABLE property_reviews;
+-- DROP TABLE property_reviews;
 
 CREATE TABLE property_reviews (
 id INTEGER PRIMARY KEY NOT NULL,
@@ -20,7 +40,7 @@ rating SMALLINT,
 message TEXT
 );
 
-DROP TABLE reservation;
+-- DROP TABLE reservation;
 
 CREATE TABLE reservation(
   id INTEGER PRIMARY KEY NOT NULL,
