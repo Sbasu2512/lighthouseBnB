8ca0c0985f7e3ae6cf1d1ddce4c805f0d30e393d
Sayantan Basu <sayantanbasu@live.in>
2021-06-24 16:58:09 -0400

    coded addProperty

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -153,9 +153,9 @@ const getAllProperties = (options, limit = 10) => {
       queryString += ` properties.cost_per_night <= $${queryParams.length} AND `;
     }
 
-
     if (queriesAdded) {
       queryString = queryString.slice(0, -4);
+      console.log(queryString);
     }
 
   }
@@ -173,7 +173,9 @@ const getAllProperties = (options, limit = 10) => {
     LIMIT $${queryParams.length};`;
 
 
-  // 6
+    console.log(queryParams);
+    console.log(queryParams.length);
+
   return pool.query(queryString, queryParams).then((res) => res.rows);
 };
 exports.getAllProperties = getAllProperties;
@@ -184,9 +186,17 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryParams = [property.owner_id,property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms, property.country, property.street, property.city, property.province, property.post_code ];
+  const queryString = `INSERT INTO properties ('owner_id','title', 'description', 'thunbnail_photo_url','cover_photo_url', 'cost_per_night', 'parking_spaces','number_of_bathrooms','number_of_bedrooms', 'country','street','city','province','post_code') VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *`;
+
+  return pool
+  .query(queryString, queryParams)
+  .then((result)=>{
+    return result.rows;
+  })
+  // const propertyId = Object.keys(properties).length + 1;
+  // property.id = propertyId;
+  // properties[propertyId] = property;
+  // return Promise.resolve(property);
 };
 exports.addProperty = addProperty;
