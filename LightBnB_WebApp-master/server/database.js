887ba22e8dfc78cb22ab99b50d8d48e97a3b3548
Sayantan Basu <sayantanbasu@live.in>
2021-06-24 14:11:16 -0400

    database.js: adduser code added

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -80,6 +80,14 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
+  const queryString = `INSERT INTO users(name, email, password)`;
+  pool
+  .query(queryString)
+  .then((result)=>{
+    console.log(result.rows);
+    return result.rows;
+  });
+  
   // const userId = Object.keys(users).length + 1;
   // user.id = userId;
   // users[userId] = user;
