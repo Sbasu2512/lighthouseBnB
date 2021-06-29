56eb015f5b38c703ab69a2016b3190002a633fe5
Sayantan Basu <sayantanbasu@live.in>
2021-06-29 14:37:45 +0000

    refactor

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -62,7 +62,7 @@ const addUser = function (user) {
   //return the promise itself
   return pool.query(queryString, values).then((result) => {
     console.log("Someone is registering...🎉🎉🎉🎉🎉🎉");
-    return result.rows;
+    return result.rows[0];
   });
 };
 exports.addUser = addUser;
@@ -89,7 +89,7 @@ const getAllReservations = function (guest_id, limit = 10) {
                        LIMIT $2;`;
 
   return pool.query(queryString, values).then((result) => {
-    console.log(result.rows);
+    // console.log(result.rows);
     return result.rows;
   });
   // return getAllProperties(null, 2);
@@ -155,7 +155,7 @@ const getAllProperties = (options, limit = 10) => {
 
     if (queriesAdded) {
       queryString = queryString.slice(0, -4);
-      console.log(queryString);
+      //console.log(queryString);
     }
 
   }
@@ -192,7 +192,7 @@ const addProperty = function (property) {
   return pool
   .query(queryString, queryParams)
   .then((result)=>{
-    return result.rows;
+    return result.rows[0];
   })
   // const propertyId = Object.keys(properties).length + 1;
   // property.id = propertyId;
@@ -200,3 +200,19 @@ const addProperty = function (property) {
   // return Promise.resolve(property);
 };
 exports.addProperty = addProperty;
+
+/**
+ * Add a reservation to the database
+ * @param {{}} reservation An object containing all of the reservation details.
+ * @returns {Promise<{}>} A promise to the reservation.
+ */
+ const addReservation = function(reservation) {
+  const reservationParams = [reservation.start_date, reservation.end_date, reservation.property_id, reservation.guest_id];
+  return db.query(`
+    INSERT INTO reservations (start_date, end_date, property_id, guest_id)
+    VALUES ($1, $2, $3, $4)
+    RETURNING *;
+  `, reservationParams)
+    .then(res => res.rows[0]);
+};
+exports.addReservation = addReservation ;
